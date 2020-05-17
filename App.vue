c1714fe3edd96a183a2a9434eb9c8b6806757ac9
practice-note <270324377@qq.com>
2020-05-17 17:49:43 +0800

    UniApp 搭建

---
+++ App.vue
@@ -0,0 +1,36 @@
+<script>
+	import { mapState } from 'vuex'
+	
+	export default {
+		onLaunch () {
+			console.log('App Launch')
+		},
+		onShow () {
+			console.log('App Show')
+		},
+		onHide () {
+			console.log('App Hide')
+		},
+		onError (err) {
+			console.warn('App Error')
+			throw new Error(err)
+		},
+		data () {
+			return {}
+		},
+		computed: {
+			...mapState({
+				loading: state => state.loading
+			})
+		},
+		watch: {
+			loading (bol) {
+				console.info('loading', bol)
+			}
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ pages/my/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="my_wrapper">
+		<text>我的</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.my_wrapper {
+	width: 100%;
+	height: 100%;
+}
+</style>
